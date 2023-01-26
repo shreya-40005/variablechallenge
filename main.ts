595b24303c960f664eaca9a8fdf5c89873867a40
shreya-40005 <122539803+shreya-40005@users.noreply.github.com>
2023-01-26 08:45:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.AB, function () {
+    counter = 0
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+    basic.showNumber(counter)
+})
+let counter = 0
+counter = 0
+basic.showNumber(counter)
